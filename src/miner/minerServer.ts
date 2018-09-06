b4dacb12a200d5972c54521d03a3c33240405be9
Francis Lim <thyeem@gmail.com>
2018-09-06 13:43:29 +0900

    back to candidateBlock interval=10s

--- src/miner/minerServer.ts
+++ src/miner/minerServer.ts
@@ -158,7 +158,7 @@ export class MinerServer {
         logger.info(`New Candidate Block Difficulty: 0x${previousDBBlock.nextDifficulty.toExponential()} Target: ${DifficultyAdjuster.getTarget(previousDBBlock.nextDifficulty, 32).toString("hex")}`)
         clearInterval(this.intervalId)
         this.createCandidate(previousDBBlock, previousHash, miner)
-        this.intervalId = setInterval(() => this.createCandidate(previousDBBlock, previousHash, miner), 15000)
+        this.intervalId = setInterval(() => this.createCandidate(previousDBBlock, previousHash, miner), 10000)
     }
     private async createCandidate(previousDBBlock: DBBlock, previousHash: Hash, miner: Address) {
         const timeStamp = Math.max(Date.now(), previousDBBlock.header.timeStamp + 50)
