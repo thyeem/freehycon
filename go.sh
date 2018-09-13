ffd40ee17e7aa1adc567cfcef16bff081172f573
lee jongwhan <leejw51@gmail.com>
2018-09-13 23:57:33 +0900

    fix go.sh

--- go.sh
+++ go.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "start stratum"
-pm2 start ./node_modules/.bin/ts-node -- src/main.ts --port=8148 --str_port=9081 --api --api_port=2442 --cpuMiners=0 --freehycon 
+pm2 start ./node_modules/.bin/ts-node -- src/miner/main.ts --port=8148 --str_port=9081 --api --api_port=2442 --cpuMiners=0 --freehycon 
