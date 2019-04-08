59b05d7b739a23820f9ccc8e04d22b667d0fbbb0
Francis <thyeem@gmail.com>
2019-04-08 17:26:01 +0900

    added README.md

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # FreeHycon Mining Pool
+
+### Background 
+
+At the early stage of [HYCON](http://explorer.hycon.io) blockchain, there were not many pools efficient. Moreover, some miners have begun to question the transparent distribution of profits because most of pools did not reveal their books at that time.
+
+[JWLee](<https://github.com/leejw51>) and I felt we need to solve this. To miners, it was the most fundamental right that should not be infringed.
+
+Based on `cluster`(*multi-core server manager*) and *publish/subscribe* of `RabbitMQ`, we implemented the following:
+
+- The most efficient *HYCON mining pool* implementation ever
+- *Transparently disclosure* all the books through the web dashboard in real-time
+- Fair profit sharing using the banker database *even if the server is in trouble*
+
+### Performance 
+
+I benchmarked it several times, it showed a pretty good mining probability. Basically it simply follows the hashrate the pool has, but the degree of pool optimization mattered due to the relatively big network latency.   
+Unfortunately, the data is gone but were shared with the miners. At that time the strong impact of `FreeHycon` is still alive in our memories :-)     
+Miners will admit it.
+
+
+### Features 
+- Fully anonymous mining
+- Fast hash rate estimation at under 10 mins
+- Fair, fair and fair: you will be properly treated as exactly to your hash rate contribution
+- For each block mined, the reward will be distributed immediately after 12 confirmations
+- Fee policy: 0.29% - 2.90% varying with time (Half-life)
+![fee](fee.svg)
+
+### How to build
+`FreeHycon` is based on [HYCON-Core](https://github.com/Team-Hycon/hycon-core) and the path below `src/miner` is the full implentation of `FreeHycon`.  
+
+Please refer to [FreeHycon installation page](<https://github.com/thyeem/freehycon_release>).
+
+### Compatibility
+
+Unfortunately, this mining pool only works under the version <= 0.0.9 (Nakamoto HYCON protocol).
+We have stopped developing it for a while. Then, there were two hard forks including the implementation of [_GHOST protocol_](https://medium.com/@teamhycon/about-ghost-protocol-44006a245ad7) and reduction in mining reward.
+`FreeHycon` does not support _Ghost Protocol_ for now but would be someday (really? -_-).  
+
+### 
+
+
--- src/main.ts
+++ src/main.ts
@@ -49,4 +49,133 @@ const optionDefinitions = [
     { name: "banker", alias: "b", type: Boolean },
 ]
 
+import conf = require("./settings")
 export const globalOptions = commandLineArgs(optionDefinitions)
+if (globalOptions.help) {
+    showHelp()
+    process.exit(0)
+}
+if (globalOptions.cpuMiners === undefined) {
+    globalOptions.cpuMiners = 1
+}
+if (globalOptions.genesis !== undefined) {
+    conf.dataGenesis = globalOptions.genesis
+}
+if (globalOptions.api_port !== "") {
+    logger.info(`API Port=${globalOptions.api_port}`)
+}
+if (globalOptions.networkid === undefined) {
+    globalOptions.networkid = "hycon"
+}
+if (globalOptions.nonLocal === undefined) {
+    globalOptions.nonLocal = false
+}
+if (globalOptions.port === 0) {
+    globalOptions.port = 20000 + Math.floor(40000 * Math.random())
+}
+if (globalOptions.postfix === undefined) {
+    globalOptions.postfix = ""
+}
+
+if (globalOptions.data === undefined) {
+    globalOptions.data = ""
+} else {
+    const target: string = globalOptions.data
+    if (target.length > 0) {
+        const lastone: string = target.slice(target.length - 1, target.length)
+        if (lastone === "/" || lastone === "\\") {
+        } else {
+            globalOptions.data += "/"
+        }
+    }
+}
+if (globalOptions.str_port === 0) {
+    globalOptions.str_port = 20000 + Math.floor(40000 * Math.random())
+}
+if (globalOptions.verbose) {
+    logger.level = "debug"
+}
+
+logger.info(`GenesisBlock=${conf.dataGenesis}`)
+logger.info(`Options=${JSON.stringify(globalOptions)}`)
+logger.info(`Verbose=${globalOptions.verbose}`)
+logger.info(`Port=${globalOptions.port}`)
+logger.info(`Stratum Port=${globalOptions.str_port}`)
+
+import * as fs from "fs-extra"
+import { Server } from "./server"
+import { Wallet } from "./wallet/wallet"
+// tslint:disable-next-line:no-var-requires
+const input = require("input")
+async function createDefaultWallet(): Promise<string> {
+    logger.warn("An encrypted mining wallet will be created for you")
+    await Wallet.walletInit()
+    let password = ""
+    while (true) {
+        const pw1 = await input.password("Please enter a password: ")
+        const pw2 = await input.password("Please confirm your password: ")
+        if (pw1 === pw2) {
+            password = pw1
+            break
+        }
+        logger.warn("Passwords do not match, please try again")
+
+    }
+    const newWallet = Wallet.randomWallet()
+    await newWallet.save("mining", password, "")
+    const address = newWallet.pubKey.address().toString()
+    logger.info(`Default Wallet: '${address}'`)
+    return address
+}
+
+export async function setMiner(address: string) {
+    globalOptions.minerAddress = conf.minerAddress = address
+    await fs.writeFileSync("./data/config.json", JSON.stringify(conf))
+}
+
+async function main() {
+    let configChange = false
+    if (globalOptions.os === undefined || globalOptions.os === "") {
+        globalOptions.os = conf.os
+    }
+    if (globalOptions.minerAddress === undefined || globalOptions.minerAddress === "") {
+        globalOptions.minerAddress = conf.minerAddress
+    }
+    if (globalOptions.minerAddress === undefined || globalOptions.minerAddress === "") {
+        try {
+            globalOptions.minerAddress = conf.minerAddress = await Wallet.getAddress("mining")
+            configChange = true
+        } catch (e) {
+
+        }
+    }
+    if (conf.txPoolMaxAddresses === undefined) {
+        conf.txPoolMaxAddresses = 36000
+        await fs.writeFileSync("./data/config.json", JSON.stringify(conf))
+    }
+    if (conf.txPoolMaxTxsPerAddress === undefined) {
+        conf.txPoolMaxTxsPerAddress = 64
+        await fs.writeFileSync("./data/config.json", JSON.stringify(conf))
+    }
+    if (globalOptions.cpuMiners > 0) {
+        if (globalOptions.minerAddress === undefined || globalOptions.minerAddress === "") {
+            try {
+                globalOptions.minerAddress = conf.minerAddress = await createDefaultWallet()
+                configChange = true
+            } catch (e) {
+                logger.error(`Failed to initialize default wallet: ${e}`)
+            }
+        }
+    }
+    if (configChange) {
+        await fs.writeFileSync("./data/config.json", JSON.stringify(conf))
+    }
+    if (globalOptions.stratum !== undefined) { return }
+    if (globalOptions.lite === undefined || globalOptions.lite) {
+        const hycon = new Server()
+        hycon.run()
+    } else {
+        throw new Error("Lite node not implemented")
+    }
+}
+main()
